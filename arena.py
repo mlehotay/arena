58bf0f3d15be9d4694504fca09dd08c87c782019
Michael Lehotay <mlehotay@gmail.com>
2020-01-22 12:52:36 -0500

    refactor classes

--- arena.py
+++ arena.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
 
-VERSION = '0.1'
-VERBOSE = False
+VERSION = '0.2'
+VERBOSE = True
 
 import random
 import sys
-
-name_list = ['Adalee', 'Amos', 'Berit', 'Bergen', 'Cavery', 'Cullan', 'Disa',
-    'Dawson', 'Edeline', 'Elias', 'Fallon', 'Fergus', 'Gemma', 'Garret',
-    'Hazel', 'Heath', 'Idelle', 'Ira', 'Jillian', 'Jasper', 'Kaia', 'Keagan',
-    'Lark', 'Landry', 'Morrin', 'Miles', 'Nella', 'Nixon', 'Odin', 'Ophelia',
-    'Patia', 'Pearce', 'Quinevere', 'Qorbin', 'Rowen', 'Reaves', 'Sabel',
-    'Soren', 'Taryn', 'Thatcher', 'Umina', 'Uri', 'Vika', 'Vance', 'Wila',
-    'Walker', 'Xenia', 'Xander', 'Yates', 'Yumi', 'Zane', 'Zinia']
 
 weapon_list = {
     None: (1,2,0),    # 1d2
@@ -35,23 +27,23 @@ def roll(dice, sides):
     return total
 
 class Fighter:
-    def __init__(self, name):
+    def __init__(self, name, faction, weapon, armor):
         self.name = name
         self.max_health = roll(2,4)
         self.health = self.max_health
-        self.weapon = None
-        self.armor = None
-        self.arena = None
-        self.team = None
+        self.faction = faction
+        self.weapon = weapon
+        self.armor = armor
+        self.battle = None
 
     def __repr__(self):
-        return f'{self.name} ({self.health}/{self.max_health}) [{self.team}]'
+        return f'{self.name} ({self.health}/{self.max_health}) [{self.faction}, {self.__class__.__name__}, {self.weapon}, {self.armor}]'
 
     def take_turn(self):
-        opponents = self.arena.fighters[:]
-        opponents.remove(self)
-        target = random.choice(opponents)
-        self.attack(target)
+        opponents = [f for f in self.battle.fighters if f.faction!=self.faction]
+        if(opponents != []):
+            target = random.choice(opponents)
+            self.attack(target)
 
     def attack(self, opponent):
         (dice, sides, plus) = weapon_list[self.weapon]
@@ -63,37 +55,50 @@ class Fighter:
         damage = max(0, damage-protection)
 
         if VERBOSE:
-            print(f'  {attacker} attacks {self} for {damage} damage')
+            print(f'  {attacker.name} attacks {self.name} for {damage} damage')
         self.health -= damage
         if(self.health < 1):
             self.die()
 
     def die(self):
         if VERBOSE:
-            print(f'  {self} dies!')
-        self.arena.fighters.remove(self)
-        self.arena = None
+            print(f'  {self.name} dies!')
+        self.battle.fighters.remove(self)
+        self.battle = None
 
 class ToughFighter(Fighter):
-    def __init__(self, name):
-        super().__init__(name)
-        self.max_health += roll(1, 8)
+    def __init__(self, name, faction, weapon, armor):
+        super().__init__(name, faction, weapon, armor)
+        self.max_health += roll(1, 10)
         self.health = self.max_health
 
-class Arena:
-    def __init__(self, fighters=[]):
+class Battle:
+    def __init__(self, title, roles):
+        self.title = title
         self.fighters = []
         self.winner = None
         self.turn = 0
-        for f in fighters:
-            self.add_fighter(f)
+        for role in roles:
+            fighter = role['class'](role['name'], role['faction'], role['weapon'], role['armor'])
+            self.add_fighter(fighter)
 
     def __repr__(self):
-        return f'Arena {hex(hash(self))} ({len(self.fighters)} fighters, turn {self.turn})'
+        return f'{self.title}, Turn {self.turn}'
 
     def add_fighter(self, fighter):
         self.fighters.append(fighter)
-        fighter.arena = self
+        fighter.battle = self
+
+    def fight_battle(self):
+        if VERBOSE:
+            print(f'{self.title} fighters:')
+            for fighter in self.fighters:
+                print(f'  {fighter}')
+        while self.winner == None:
+            self.play_round()
+        if VERBOSE:
+            print(f'{self.winner} wins {self.title}!')
+        return self.winner
 
     def play_round(self):
         self.turn += 1
@@ -101,86 +106,46 @@ class Arena:
             print(f'{self}:')
         for f in self.fighters:
             f.take_turn()
-        if len(self.fighters) == 1:
-            self.winner = self.fighters[0]
+        if len({f.faction for f in self.fighters}) == 1:
+            self.winner = self.fighters[0].faction
 
-class Team:
-    def __init__(self, name, fighter_type, weapon, armor):
-        self.name = name
-        self.fighter_type = fighter_type
-        self.type_name = fighter_type.__name__
-        self.weapon = weapon
-        self.armor = armor
+class Arena:
+    def __init__(self, roles, iterations=1000, verbose=False):
+        self.roles = roles
+        self.iterations = iterations
+        self.verbose = verbose
+        self.factions = {role['faction'] for role in roles}
+        self.wins = {faction:0 for faction in self.factions}
+        self.winner = None
 
-    def __repr__(self):
-        return f'{self.name} ({self.type_name}, {self.weapon}, {self.armor})'
+    def simulate_battle(self):
+        for i in range(0, self.iterations):
+            winner = Battle(f'Battle {i+1}', self.roles).fight_battle()
+            self.wins[winner] += 1
 
-    def add_fighter(self, fighter):
-        fighter.team = self
-        fighter.weapon = self.weapon
-        fighter.armor = self.armor
-
-class Tournament:
-    def __init__(self, teams):
-        self.teams = teams
-        self.wins = {team:0 for team in teams}
-
-    def __repr__(self):
-        return f'Tournament {hex(hash(self))} {[team.type_name for team in self.teams]}'
-
-    def fight_battle(self, title, teams):
-        fighters = []
-        for team in self.teams:
-            name = random.choice(name_list)
-            fighter = team.fighter_type(name)
-            team.add_fighter(fighter)
-            fighters.append(fighter)
-        if VERBOSE:
-            print(f'{self} {title}:')
-        arena = Arena(fighters)
-        while arena.winner == None:
-            arena.play_round()
-        if VERBOSE:
-            print(f'{arena.winner} wins {title}!')
-        return arena.winner.team
-
-    def compete(self, num_battles):
-        for i in range(0, num_battles):
-            winning_team = self.fight_battle(f'Battle {i+1}', self.teams)
-            self.wins[winning_team] += 1
-
-        most_wins = max([self.wins[team] for team in self.teams])
-        winning_teams = [team for team in self.teams if self.wins[team]==most_wins]
-        if len(winning_teams)>1:
-            winning_team = self.fight_battle('Tiebreaker Battle', winning_teams)
-            self.wins[winning_team] += 1
-            self.winner = winning_team
-        else:
-            self.winner = winning_teams[0]
-
-        if VERBOSE:
-            print(f'{self.winner} wins the tournament!')
-
-    def print_standings(self):
-        print('Final Standings:')
-        for team in self.teams:
-            print(f'  {team}: {self.wins[team]}')
+    def print_probabilities(self):
+        print('Estimated Probabilities of Victory:')
+        for faction in self.factions:
+            print(f'  {faction}: {self.wins[faction]/self.iterations}')
 
 class Game:
     def run(self, argv):
         print('Arena version ' + VERSION)
 
-        teams = [
-            Team('Flying Gryphons', Fighter, 'sword', 'chain mail'),
-            Team('Mob Riot', ToughFighter, None, 'leather armor'),
-            Team('Dungeon Explorers', Fighter, None, None),
-            Team('Pointy Things', Fighter, 'dagger', 'leather armor'),
-            Team('Bludgeoners', ToughFighter, 'mace', None)
+        roles = [
+            {'name': 'Alice', 'faction': 'Order',
+                'class':Fighter, 'weapon':'sword', 'armor':'chain mail'},
+            {'name': 'Bob', 'faction': 'Order',
+                'class':ToughFighter, 'weapon':None, 'armor':'leather armor'},
+            {'name': 'Eve', 'faction': 'Chaos',
+                'class':Fighter, 'weapon':'dagger', 'armor':None},
+            {'name': 'Mallory', 'faction': 'Chaos',
+                'class':ToughFighter, 'weapon':'mace', 'armor':'leather armor'}
         ]
 
-        t = Tournament(teams)
-        t.compete(1000)
-        t.print_standings()
+        battle = Arena(roles, iterations=2)
+        battle.simulate_battle()
+        battle.print_probabilities()
 
 if __name__ == '__main__':
     Game().run(sys.argv)
